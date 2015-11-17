470f243db39cc9a1656b73c94aaebe5556371faa
Adrian Juarez <aejuarez@outlook.com>
2015-11-16 22:01:49 -0800

    update javascript

--- js/validate-contact-form.js
+++ js/validate-contact-form.js
@@ -1,1 +1,27 @@
-$("#contact-form").validate();
+(function() {
+    var $contactForm = $('#contactForm');
+    // VALIDATE FORM
+    $contactForm.validate();
+    $contactForm.submit(function(e) {
+        if ($(this).valid()) {
+        e.preventDefault();
+        $.ajax({
+            url: '//formspree.io/myemail@mail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                $contactForm.append('<div class="message message--loading">Sending message…</div>');
+            },
+            success: function(data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--success">Message sent!</div>');
+            },
+            error: function(err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--error">Ops, there was an error.</div>');
+            }
+        });}
+       return false;
+     });
+})();
